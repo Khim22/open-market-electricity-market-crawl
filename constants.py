c191c49f0c749fc80d35ffd2da17afe63e5db226
Khim22 <khimyang22@gmail.com>
2019-04-11 18:32:19 +0800

    (feat): added list of constants in constants.py

--- constants.py
+++ constants.py
@@ -1,4 +1,21 @@
-housing_types =  ["HDB 1-Room", "HDB 2-Room", "HDB 3-Room", "HDB 4-Room",
+housing_types =  [ "HDB 1-Room", "HDB 2-Room", "HDB 3-Room", "HDB 4-Room",
                  "HDB 5-Room", "HDB Executive", "Apartment", "Terrace",
                   "Semi-Detached", "Bungalow" ]
 
+consumer_types = [ "Residential", "Business" ]
+
+price_plan_types = [ "Fixed Price", "Discount off Regulated Tariff" ]
+
+retailers = ["Best Electricity Supply Pte Ltd",
+            'Diamond Electric (by Diamond Energy Merchants Pte Ltd)',
+            'ES Power (by Environmental Solutions (Asia) Pte Ltd)',
+            'iSwitch Pte Ltd',
+            'Keppel Electric Pte Ltd',
+            'Ohm Energy Pte Ltd',
+            'PacificLight Energy Pte Ltd',
+            'Sembcorp Power Pte Ltd',
+            'Senoko Energy Supply Pte Ltd',
+            'Seraya Energy Pte Ltd (Geneco)',
+            'Sunseap Energy Pte Ltd',
+            'Tuas Power Supply Pte Ltd'
+            ]
--- gui.py
+++ gui.py
@@ -11,12 +11,16 @@ def create_title_frame(root):
     titleFrame.rowconfigure(0, weight=1)
     return titleFrame
 
-def create_option_menu_row(label_text, list, row_num, container_frame):
+def create_row_frame(container_frame, row_num, label_text):
     rowFrame = Frame(container_frame)
     rowFrame.grid(column=0, row=row_num, padx=10, pady=10)
     label = Label(rowFrame, text=label_text, font=FONT, anchor="w")
     label.grid(row=0, column=0)
+    return rowFrame
 
+
+def create_option_menu_row(label_text, list, row_num, container_frame):
+    rowFrame = create_row_frame(container_frame,row_num, label_text)
     variable = StringVar(rowFrame)
     variable.set(list[0]) # default value
 
@@ -27,26 +31,32 @@ def create_option_menu_row(label_text, list, row_num, container_frame):
     w.grid(row=0, column=1, columnspan=2)
 
 def create_input_row(label_text, row_num, container_frame):
-    rowFrame = Frame(container_frame)
-    rowFrame.grid(column=0, row=row_num, padx=10, pady=10)
-    label = Label(rowFrame, text=label_text, font=FONT, anchor="w")
-    label.grid(row=0, column=0)
+    rowFrame = create_row_frame(container_frame,row_num, label_text)
 
     input_field = Entry(rowFrame, bd=2, textvariable= IntVar())
     input_field.grid(row=0, column=1, columnspan=2)
     return input_field
+
+def create_list_box_row(label_text, list, row_num, container_frame):
+    rowFrame = create_row_frame(container_frame,row_num, label_text)
+    listbox = Listbox(rowFrame, selectmode=MULTIPLE, height=5)
+    listbox.insert(0, *list)
+    listbox.grid(row=0, column=1)
+    scrollbar = Scrollbar(rowFrame, orient="vertical")
+    scrollbar.config(command=listbox.yview)
+    scrollbar.grid(row=0, column=2)
+    listbox.config(yscrollcommand=scrollbar.set)
+    return listbox
 
 def submit(control, label):
     value = control.get()
     label.config(text=value)
     # print(g)
 
-def onclick():
-    label.config(text="value")
 
 root = Tk()
 root.title('Open Market Electricity Crawl')
-root.geometry('650x350')
+root.geometry('650x650')
 
 titleFrame = create_title_frame(root)
 
@@ -55,7 +65,7 @@ title.grid(row=1, column=0, columnspan=3, padx=20, pady=20)
 
 create_option_menu_row("Consumer Type", constants.housing_types, 3, titleFrame)
 create_option_menu_row("Housing Type", constants.housing_types, 4, titleFrame)
-create_option_menu_row("Price Plan Type", constants.housing_types, 5, titleFrame)
+create_list_box_row("Price Plan Type", constants.housing_types, 5, titleFrame)
 create_option_menu_row("Retailer", constants.housing_types, 6, titleFrame)
 monthly_consumption_field = create_input_row("Average Monthly Consumption(kWh)", 7, titleFrame)
 
